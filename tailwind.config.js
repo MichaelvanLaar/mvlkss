a5e2b6e1056962b8db00824308633a6aa803b61d
Michael van Laar <michael@van-laar.de>
2023-04-16 21:28:03 +0200

    feat(Tailwind CSS): :lipstick: Add tailwindcss-fluid-type plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+  plugins: [
+    require("tailwindcss-fluid-type"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
 };
