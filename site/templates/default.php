4bb0f6863b4da508a65108bfb9303321bfd17c0c
Michael van Laar <michael@van-laar.de>
2023-05-10 02:03:39 +0200

    🎨 style(default.php): add page header and footer sections to improve page layout The changes add a page header and footer section to the default template. The header and footer sections are wrapped in a container with a default margin and padding. The main section is also wrapped in a container with a default margin and padding. This improves the layout of the page and makes it easier to add content to the header and footer sections.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,5 +1,24 @@
 <?php snippet('header') ?>
 
-    <h1><?= $page->title() ?></h1>
+    <!-- Page header -->
+    <div>
+      <div class="mvl-container-default">
+        <p>Here is room for the page header.</p>
+      </div>
+    </div>
+
+    <!-- Main section -->
+    <div class="flex-grow">
+      <div class="mvl-container-default js-page-main-content">
+        <h1><?= $page->title() ?></h1>
+      </div>
+    </div>
+
+    <!-- Page footer -->
+    <div>
+      <div class="mvl-container-default">
+        <p>Here is room for the page footer.</p>
+      </div>
+    </div>
 
 <?php snippet('footer') ?>
