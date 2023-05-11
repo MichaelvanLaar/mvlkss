05fb8ffef9f5ecec2417609b179c74b5c31acbb6
Michael van Laar <michael@van-laar.de>
2023-05-12 00:08:33 +0200

    🎨 style(site.yml): change defaultDevider to defaultDivider and update label casing 🚀 feat(site.yml): add twitterSiteHandle field to site blueprint 🐛 fix(header.controller.php): pass $site and $page variables to the controller function 🚀 feat(header.controller.php): add logic to append site title and default divider to meta and social share titles 🚀 feat(header.controller.php): add pageLanguageLocale, metaTitle, socialShareTitle, socialShareDescription, and twitterSiteHandle variables to the returned array 🚀 feat(header.php): add Open Graph and Twitter Card metadata to the header The defaultDevider field in the site blueprint was renamed to defaultDivider to improve semantics and the label casing was updated. The twitterSiteHandle field was added to the site blueprint to allow for the specification of a Twitter handle for the site. The header.controller.php file was updated to pass the $site and $page variables to

--- site/snippets/header.controller.php
+++ site/snippets/header.controller.php
@@ -2,16 +2,50 @@
 /**
  * =============================================================================
  * Controller for Header snippet (which is used on all pages)
- * 
+ *
  * Uses the Kirby Snippet Controller plugin
  * Plugin details: https://github.com/lukaskleinschmidt/kirby-snippet-controller
  * =============================================================================
  */
 
-return function ($kirby) {
+return function ($kirby, $site, $page) {
+  // Meta title (for <title> element)
+  $metaTitle =
+    $page->seoTitle()->length() > 0 ? $page->seoTitle() : $page->title();
+  $metaTitle = $page->appendDefaultDividerToTitleElement()->toBool()
+    ? $metaTitle . " " . $site->defaultDivider()
+    : $metaTitle;
+  $metaTitle = $page->appendSiteTitleToTitleElement()->toBool()
+    ? $metaTitle . " " . $site->title()
+    : $metaTitle;
+
+  // Social share title (for Open Graph and Twitter Card title)
+  $socialShareTitle =
+    $page->socialShareTitle()->length() > 0
+      ? $page->socialShareTitle()
+      : $page->title();
+  $socialShareTitle = $page->appendDefaultDividerToSocialShareTitle()->toBool()
+    ? $socialShareTitle . " " . $site->defaultDivider()
+    : $socialShareTitle;
+  $socialShareTitle = $page->appendSiteTitleToSocialShareTitle()->toBool()
+    ? $socialShareTitle . " " . $site->title()
+    : $socialShareTitle;
+
   return [
     "pageLanguageCode" => $kirby->language()
       ? $kirby->language()->code()
       : "en",
+    "pageLanguageLocale" => $kirby->language()
+      ? $kirby->language()->locale()
+      : "en_US",
+    "metaTitle" => $metaTitle,
+    "socialShareTitle" => $socialShareTitle,
+    "socialShareDescription" =>
+      $page->socialShareDescription()->length() > 0
+        ? $page->socialShareDescription()
+        : "",
+    "twitterSiteHandle" => $site->twitterSiteHandle()->length() > 0
+      ? $site->twitterSiteHandle()
+      : "",
   ];
 };
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -8,6 +8,11 @@
  * 
  * Variables form controller:
  * - $pageLanguageCode
+ * - pageLanguageLocale
+ * - $metaTitle
+ * - $socialShareTitle
+ * - $socialShareDescription
+ * - $twitterSiteHandle
  * =============================================================================
  */
 ?>
@@ -16,7 +21,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?= $page->title() ?> | <?= $site->title() ?></title>
+    <title><?= $metaTitle ?></title>
 
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
@@ -34,6 +39,22 @@
       })(document.documentElement);
     </script>
 
-    <?= css("assets/css/main.css") ?>    
+    <?= css("assets/css/main.css") ?>
+
+    <!-- Open Graph Data -->
+    <meta property="og:title" content="<?= $socialShareTitle ?>" />
+    <meta property="og:description" content="<?= $socialShareDescription ?>" />
+    <!-- <meta property="og:image" content="[Insert URL of image with 2:1 ratio]" /> -->
+    <meta property="og:locale" content="<?= $pageLanguageLocale ?>" />
+
+    <!-- Twitter Data -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <?php if (strlen($twitterSiteHandle) > 0): ?>
+      <meta name="twitter:site" content="<?= $twitterSiteHandle ?>" />
+    <?php endif; ?>
+    <!-- <meta name="twitter:creator" content="[Insert @user_name_of_content_creator]" /> -->
+    <meta name="twitter:title" content="<?= $socialShareTitle ?>" />
+    <meta name="twitter:description" content="<?= $socialShareDescription ?>" />
+    <!-- <meta name="twitter:image" content="[Insert URL of image with 2:1 ratio]" /> -->
   </head>
   <body class="flex flex-col min-h-screen">
