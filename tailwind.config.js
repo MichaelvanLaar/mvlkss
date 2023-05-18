ab9cc3adbc6b11aa964e27e7fe29f2a9e2427c32
Michael van Laar <michael@van-laar.de>
2023-05-18 16:56:21 +0200

    chore(tailwind.config.js): add custom plugins for js and no-js variants to modify selectors based on the presence of js class in the DOM.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: ["./site/**/*.php"],
+
   theme: {
     extend: {
       screens: {
-        "screen": {"raw": "screen"},
+        screen: { raw: "screen" },
       },
     },
   },
+
   plugins: [
     require("@tailwindcss/container-queries"),
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
+
+    // Cusotm plugins
+    plugin(function ({ addVariant, e }) {
+      // Add “js:” variant
+      addVariant("js", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.js .${e(`js${separator}${className}`)}`;
+        });
+      });
+
+      // Add “no-js:” variant
+      addVariant("no-js", ({ modifySelectors, separator }) => {
+        modifySelectors(({ className }) => {
+          return `.no-js .${e(`no-js${separator}${className}`)}`;
+        });
+      });
+    }),
   ],
 };
