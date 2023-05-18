c8fc01f97070fe796c76b0d95e8cf96fb682742e
Michael van Laar <michael@van-laar.de>
2023-05-18 17:47:41 +0200

    style(default.php): remove unnecessary divs and classes from default template to simplify HTML structure and improve readability

--- site/templates/default.php
+++ site/templates/default.php
@@ -2,15 +2,8 @@
 
     <!-- Main section -->
     <div class="flex-grow">
-      <div class="mvl-container-default js-page-main-content">
+      <div class="row-container-default js-page-main-content">
         <h1><?= $page->title() ?></h1>
-      </div>
-    </div>
-
-    <!-- Page footer -->
-    <div>
-      <div class="mvl-container-default">
-        <p>Here is room for the page footer.</p>
       </div>
     </div>
 
