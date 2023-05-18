949ce696d35ca0a3b51e09b8dbdf8b837114a5f7
Michael van Laar <michael@van-laar.de>
2023-05-18 17:47:57 +0200

    feat(footer.php): add page footer to improve user experience and provide additional information to the user

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,6 +5,13 @@
  * =============================================================================
  */
 ?>
+    <!-- Page footer -->
+    <div>
+      <div class="row-container-default">
+        <p>Here is room for the page footer.</p>
+      </div>
+    </div>
+
     <?= js("assets/js/main.js") ?>
   </body>
 </html>
