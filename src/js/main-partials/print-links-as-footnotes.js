72e6128ce42424ac05be3f791ec6ca4fc7362d69
Michael van Laar <michael@van-laar.de>
2023-05-29 20:14:30 +0200

    🔨 refactor(print-links-as-footnotes.js): refactor printLinksAsFootnotes function into an IIFE and add configuration constants ✨ feat(print-links-as-footnotes.js): add support for printing links as footnotes with reference numbers The printLinksAsFootnotes function has been refactored into an Immediately Invoked Function Expression (IIFE) to improve encapsulation and readability. Configuration constants have been added to make it easier to customize the behavior of the function. The function now supports printing links as footnotes with reference numbers. The reference numbers are automatically generated and inserted next to each link. A list of footnotes is also created and appended to the end of the document body.

--- src/js/main-partials/print-links-as-footnotes.js
+++ src/js/main-partials/print-links-as-footnotes.js
@@ -4,139 +4,149 @@
  * =============================================================================
  */
 
-/**
- * -----------------------------------------------------------------------------
- * Main function
- * -----------------------------------------------------------------------------
- */
+(function (linksContainerSelector) {
+  /**
+   * ---------------------------------------------------------------------------
+   * Configuration
+   * ---------------------------------------------------------------------------
+   */
 
-function printLinksAsFootnotes(
-  linksContainerSelector,
-  footnotesContainerClassName = "print-links-footer",
-  printOnlyClassName = "print-only",
-  footnotesHeadlineElement = "h3",
-  footnotesHeadlineText = "Links",
-  referenceTextBeforeNumber = "[",
-  referenceTextAfterNumber = "]"
-) {
-  // Cancel if no links container selector was provided or if no links container
-  // was found
-  if (!linksContainerSelector) return;
-  const linksContainer = document.querySelector(linksContainerSelector);
-  if (!linksContainer) return;
+  // To configure the container which contains all links that should be
+  // processed, change the parameter in the last linke of this file.
 
-  // Get links and process them
-  const links = getLinks(linksContainerSelector);
-  const processedLinks = processLinks(
-    links,
+  const footnotesContainerClassName = "print-links-footer";
+  const printOnlyClassName = "print-only";
+  const footnotesHeadlineElement = "h3";
+  const footnotesHeadlineText = "Links";
+  const referenceTextBeforeNumber = "[";
+  const referenceTextAfterNumber = "]";
+
+  /**
+   * ---------------------------------------------------------------------------
+   * Helper functions
+   * ---------------------------------------------------------------------------
+   */
+
+  // Create the footnotes container with a headline
+  function createFootnotesContainer(
+    className,
     printOnlyClassName,
-    referenceTextBeforeNumber,
-    referenceTextAfterNumber
-  );
+    headlineElement,
+    headlineText
+  ) {
+    const container = document.createElement("section");
+    container.classList.add(className, printOnlyClassName);
+    container.innerHTML = `<${headlineElement}>${headlineText}</${headlineElement}>`;
+    return container;
+  }
 
-  // Cancel if no links were found
-  if (processedLinks.length == 0) return;
+  // Get all links that match the selector and do not start with “#”, “mailto”,
+  // or “javascript”
+  function getLinks(selector) {
+    return Array.from(
+      document.querySelectorAll(
+        `${selector} a[href]:not([href^='#']):not([href^="mailto"]):not([href^="javascript"])`
+      )
+    );
+  }
 
-  // Create footnotes container and list
-  const footnotesContainer = createFootnotesContainer(
-    footnotesContainerClassName,
-    printOnlyClassName,
-    footnotesHeadlineElement,
-    footnotesHeadlineText
-  );
-  const footnotesList = document.createElement("ol");
-  footnotesList.classList.add("print-links-footer-list");
+  // Process the links, add reference numbers and return an array of processed
+  // links
+  function processLinks(links, printOnlyClassName, beforeNumber, afterNumber) {
+    const urlMap = new Map();
 
-  // Add footnotes to the list
-  processedLinks.forEach(({ url, isDuplicate }) => {
-    if (!isDuplicate) {
-      footnotesList.appendChild(createFootnoteListItem(url));
-    }
-  });
+    return links
+      .filter((link) => link.getElementsByTagName("img").length < 1)
+      .map((link) => {
+        const url = link.href;
+        let refNumber = urlMap.get(url);
+        const isDuplicate = refNumber !== undefined;
 
-  // Append footnotes list to the container and add it to the body
-  footnotesContainer.appendChild(footnotesList);
-  document.body.appendChild(footnotesContainer);
-}
+        if (!isDuplicate) {
+          refNumber = urlMap.size + 1;
+          urlMap.set(url, refNumber);
+        }
 
-/**
- * -----------------------------------------------------------------------------
- * Helper functions
- * -----------------------------------------------------------------------------
- */
+        // Create and insert the reference number
+        const reference = createReference(
+          refNumber,
+          printOnlyClassName,
+          beforeNumber,
+          afterNumber
+        );
+        link.parentNode.insertBefore(reference, link.nextSibling);
 
-// Create the footnotes container with a headline
-function createFootnotesContainer(
-  className,
-  printOnlyClassName,
-  headlineElement,
-  headlineText
-) {
-  const container = document.createElement("section");
-  container.classList.add(className, printOnlyClassName);
-  container.innerHTML = `<${headlineElement}>${headlineText}</${headlineElement}>`;
-  return container;
-}
+        return { url, isDuplicate };
+      });
+  }
 
-// Get all links that match the selector and do not start with “#”, “mailto”, or
-// “javascript”
-function getLinks(selector) {
-  return Array.from(
-    document.querySelectorAll(
-      `${selector} a[href]:not([href^='#']):not([href^="mailto"]):not([href^="javascript"])`
-    )
-  );
-}
+  // Create a reference element with the provided number and text
+  function createReference(number, className, beforeText, afterText) {
+    const reference = document.createElement("sup");
+    reference.classList.add(className);
+    reference.textContent = `${beforeText}${number}${afterText}`;
+    return reference;
+  }
 
-// Process the links, add reference numbers and return an array of processed
-// links
-function processLinks(links, printOnlyClassName, beforeNumber, afterNumber) {
-  const urlMap = new Map();
+  // Create a list item for the footnote with the provided URL
+  function createFootnoteListItem(url) {
+    const listItem = document.createElement("li");
+    listItem.textContent = url;
+    return listItem;
+  }
 
-  return links
-    .filter((link) => link.getElementsByTagName("img").length < 1)
-    .map((link) => {
-      const url = link.href;
-      let refNumber = urlMap.get(url);
-      const isDuplicate = refNumber !== undefined;
+  /**
+   * ---------------------------------------------------------------------------
+   * Main function
+   * ---------------------------------------------------------------------------
+   */
 
+  function printLinksAsFootnotes() {
+    // Cancel if no links container selector was provided or if no links
+    // container was found
+    if (!linksContainerSelector) return;
+    const linksContainer = document.querySelector(linksContainerSelector);
+    if (!linksContainer) return;
+
+    // Get links and process them
+    const links = getLinks(linksContainerSelector);
+    const processedLinks = processLinks(
+      links,
+      printOnlyClassName,
+      referenceTextBeforeNumber,
+      referenceTextAfterNumber
+    );
+
+    // Cancel if no links were found
+    if (processedLinks.length == 0) return;
+
+    // Create footnotes container and list
+    const footnotesContainer = createFootnotesContainer(
+      footnotesContainerClassName,
+      printOnlyClassName,
+      footnotesHeadlineElement,
+      footnotesHeadlineText
+    );
+    const footnotesList = document.createElement("ol");
+    footnotesList.classList.add("print-links-footer-list");
+
+    // Add footnotes to the list
+    processedLinks.forEach(({ url, isDuplicate }) => {
       if (!isDuplicate) {
-        refNumber = urlMap.size + 1;
-        urlMap.set(url, refNumber);
+        footnotesList.appendChild(createFootnoteListItem(url));
       }
-
-      // Create and insert the reference number
-      const reference = createReference(
-        refNumber,
-        printOnlyClassName,
-        beforeNumber,
-        afterNumber
-      );
-      link.parentNode.insertBefore(reference, link.nextSibling);
-
-      return { url, isDuplicate };
     });
-}
 
-// Create a reference element with the provided number and text
-function createReference(number, className, beforeText, afterText) {
-  const reference = document.createElement("sup");
-  reference.classList.add(className);
-  reference.textContent = `${beforeText}${number}${afterText}`;
-  return reference;
-}
+    // Append footnotes list to the container and add it to the body
+    footnotesContainer.appendChild(footnotesList);
+    document.body.appendChild(footnotesContainer);
+  }
 
-// Create a list item for the footnote with the provided URL
-function createFootnoteListItem(url) {
-  const listItem = document.createElement("li");
-  listItem.textContent = url;
-  return listItem;
-}
+  /**
+   * ---------------------------------------------------------------------------
+   * Apply main function
+   * ---------------------------------------------------------------------------
+   */
 
-/**
- * -----------------------------------------------------------------------------
- * Apply main function
- * -----------------------------------------------------------------------------
- */
-
-printLinksAsFootnotes(".js-page-main-content");
+  printLinksAsFootnotes();
+})(".js-page-main-content"); // Call the IIFE and pass the parameter
