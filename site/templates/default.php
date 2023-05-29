e134f52225881644a774e073362b38e43dd4d211
Michael van Laar <michael@van-laar.de>
2023-05-30 00:50:22 +0200

    🐛 fix(default.php): fix typo in CSS class causing layout issues The CSS class `mt-[calc(var(--site-header-initial-height))]` was causing layout issues due to a typo. The `+_` was missing between `var(--site-header-initial-height)` and `1.5rem`. This commit fixes the typo by adding the missing `+_` to the class.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
     <!-- Main section -->
-    <div class="flex-grow mt-[calc(var(--site-header-initial-height))] js-page-main-content">
+    <div class="flex-grow mt-[calc(var(--site-header-initial-height)_+_1.5rem)] js-page-main-content">
       <div><!-- Row -->
         <div class="row-container-default"><!-- Inner row container -->
           <h1><?= $page->title() ?></h1>
