6574ce74b715cf0fbea329c312e01f047f1986de
Michael van Laar <michael@van-laar.de>
2023-05-31 02:05:05 +0200

    🔨 refactor(shrink-page-header-on-scroll.js): wrap entire code inside an IIFE to prevent global scope pollution 🔨 refactor(shrink-page-header-on-scroll.js): add comments to improve code readability ✨ feat(shrink-page-header-on-scroll.js): add support for dynamic header height and vertical padding on scroll The entire code is now wrapped inside an Immediately Invoked Function Expression (IIFE) to prevent any variables or functions defined inside from polluting the global scope. Comments were added to improve code readability. The script now supports dynamic header height and vertical padding on scroll, which improves the user experience by making the header more compact and less intrusive as the user scrolls down the page.

--- src/js/main-partials/shrink-page-header-on-scroll.js
+++ src/js/main-partials/shrink-page-header-on-scroll.js
@@ -4,6 +4,9 @@
  * =============================================================================
  */
 
+// Wrap the entire code inside an Immediately Invoked Function Expression
+// (IIFE). This will prevent any variables or functions defined inside from
+// polluting the global scope.
 (function () {
   /**
    * ---------------------------------------------------------------------------
@@ -27,8 +30,11 @@
    * ---------------------------------------------------------------------------
    */
 
+  // Select the body element
   const body = document.body;
 
+  // Retrieve the custom CSS property values from the body element’s computed
+  // style.
   const initialHeaderHeight = parseFloat(
     getComputedStyle(body).getPropertyValue("--site-header-initial-height")
   );
@@ -48,20 +54,34 @@
    * ---------------------------------------------------------------------------
    */
 
+  // Add a scroll event listener to the window. This function is called every
+  // time the user scrolls.
   window.addEventListener("scroll", () => {
-    const scrollPosition = window.pageYOffset;
+    // Get the current scroll position.
+    const scrollPosition = window.scrollY;
 
+    // Initialize the header’s height and vertical padding to their initial
+    // values.
     let headerHeight = initialHeaderHeight;
     let headerPaddingY = initialHeaderPaddingY;
 
+    // If the scroll position is greater than the scroll threshold, start
+    // modifying the header’s height and vertical padding.
     if (scrollPosition > scrollThreshold) {
+      // Calculate how far the user scrolled since passing the scroll threshold,
+      // but don’t exceed the maximum scroll distance.
       const scrollDistance = Math.min(
         scrollPosition - scrollThreshold,
         maxScrollDistance
       );
 
+      // Calculate the ratio of the scroll distance to the maximum scroll
+      // distance.
       const scrollRatio = scrollDistance / maxScrollDistance;
 
+      // Calculate the new values for the header’s height and vertical padding
+      // by interpolating between their initial and scroll values based on the
+      // scroll ratio.
       headerHeight =
         initialHeaderHeight -
         (initialHeaderHeight - scrollHeaderHeight) * scrollRatio;
@@ -70,6 +90,7 @@
         (initialHeaderPaddingY - scrollHeaderPaddingY) * scrollRatio;
     }
 
+    // Update the custom CSS properties on the body element with the new values.
     body.style.setProperty("--site-header-height", `${headerHeight}rem`);
     body.style.setProperty("--site-header-padding-y", `${headerPaddingY}rem`);
   });
