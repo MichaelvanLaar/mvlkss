01ddc8126f511f2d40f2165df65abac60f2adfd8
Michael van Laar <michael@van-laar.de>
2023-06-01 01:17:21 +0200

    🎨 style(default.php): reformat HTML code to improve readability The HTML code has been reformatted to improve readability and make it easier to understand the structure of the page. The changes include adding new lines and indentation to separate different elements and adding comments to describe the purpose of each section.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,9 +1,14 @@
 <?php snippet('header') ?>
 
     <!-- Main section -->
-    <div class="flex-grow js:mt-[calc(var(--site-header-initial-height)_+_1.5rem)] no-js:mt-6 js-page-main-content">
-      <div><!-- Row -->
-        <div class="row-container-default"><!-- Inner row container -->
+    <div
+      id="page-main-section"
+      class="js-page-main-content flex-grow js:mt-[calc(var(--site-header-initial-height)_+_1.5rem)] no-js:mt-6"
+    >
+      <!-- Row -->  
+      <div>
+        <!-- Inner row container -->
+        <div class="row-container-default">
           <h1><?= $page->title() ?></h1>
           <?= $page->text()->kt() ?>
         </div>
