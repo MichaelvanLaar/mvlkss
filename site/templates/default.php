b2e8787907a5a25bfe6e5b661be984b57fcbe255
Michael van Laar <michael@van-laar.de>
2023-06-04 01:09:48 +0200

    🎨 style(default.php): change comment to uppercase for consistency The comment was changed to uppercase to improve consistency with the naming conventions used in the rest of the codebase.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,6 +1,6 @@
 <?php snippet('header') ?>
 
-    <!-- Main section -->
+    <!-- MAIN SECTION -->
     <div
       id="page-main-section"
       class="js-page-main-content flex-grow js:mt-[calc(var(--site-header-initial-height)_+_1.5rem)] no-js:mt-6"
