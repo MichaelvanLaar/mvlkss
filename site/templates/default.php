f812b763f36042d50b6a84974eea0f4f04426429
Michael van Laar <michael@van-laar.de>
2023-06-05 02:26:41 +0200

    🎨 style(default.php): refactor default template to use snippets for header and footer 🚀 feat(default.php): add page-builder-default snippet to default template to improve code reusability and readability The default template has been refactored to use snippets for the header and footer, which improves code reusability and readability. The page-builder-default snippet has been added to the template to improve code reusability and readability.

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,18 +1,12 @@
-<?php snippet('header') ?>
+<?php snippet("base/header"); ?>
 
     <!-- MAIN SECTION -->
     <div
       id="page-main-section"
       class="js-page-main-content flex-grow js:mt-[calc(var(--site-header-initial-height)_+_1.5rem)] no-js:mt-6"
     >
-      <!-- Row -->  
-      <div>
-        <!-- Inner row container -->
-        <div class="row-container-default">
-          <h1><?= $page->title() ?></h1>
-          <?= $page->text()->kt() ?>
-        </div>
-      </div>
+      <?php snippet("page-builder-default") ?>
+
     </div>
 
-<?php snippet('footer') ?>
+<?php snippet("base/footer"); ?>
