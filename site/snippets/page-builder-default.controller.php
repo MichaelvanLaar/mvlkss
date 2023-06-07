3bf41b6fdc5f2df7035e00e055ef6ff24d9de3dd
Michael van Laar <michael@van-laar.de>
2023-06-08 00:57:17 +0200

    🚀 feat(page-builder-default): add controller to improve code organization and readability The new controller file for the Page Builder (default) Snippet uses the Kirby Snippet Controller plugin to provide variables for use in the header snippet. The controller file also processes rows and columns, and returns the variables to the snippet. This improves code organization and readability. The snippet file has been updated to receive the variables from the controller. The code has also been refactored to use an array of column width classes instead of a switch statement.

---
+++ site/snippets/page-builder-default.controller.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * =============================================================================
+ * Controller for Page Builder (default) Snippet
+ *
+ * Uses the Kirby Snippet Controller plugin
+ * Plugin details: https://github.com/lukaskleinschmidt/kirby-snippet-controller
+ *
+ * Provides variables for use in the header snippet:
+ * - $layoutRowsData
+ * =============================================================================
+ */
+
+return function ($page) {
+  /**
+   * ---------------------------------------------------------------------------
+   * Configuration
+   * ---------------------------------------------------------------------------
+   */
+
+  // Set vertical padding values for the “small”, “medium” and “large” options of the
+  // respective layout settings field using Tailwind CSS classes
+  $layoutRowVerticalPaddingValues = [
+    "small" => "py-3",
+    "medium" => "py-6",
+    "large" => "py-12",
+    "huge" => "py-24"
+  ];
+
+  /**
+   * ---------------------------------------------------------------------------
+   * Processing rows and columns
+   * ---------------------------------------------------------------------------
+   */
+  $layoutRows = $page->mainContent()->toLayouts();
+  $layoutRowsData = [];
+
+  foreach ($layoutRows as $layoutRow) {
+    // Construct the ID attribute for the current row
+    $layoutRowId = $layoutRow->rowId()->isNotEmpty()
+      ? " id=\"" . $layoutRow->rowId() . "\""
+      : "";
+
+    // Set the vertical padding related CSS class for the current row
+    $layoutRowVerticalPaddingKey = (string) $layoutRow->rowVerticalPadding();
+    $rowVerticalPaddingClass =
+      $layoutRowVerticalPaddingValues[$layoutRowVerticalPaddingKey] ?? "py-0";
+
+    // Set the column splitting related CSS class for the current row
+    $layoutColumnSplitting = "column-splitting-";
+    foreach ($layoutRow->columns() as $layoutColumn) {
+      $layoutColumnSplitting .= $layoutColumn->width() . "-";
+    }
+    $layoutColumnSplitting = rtrim($layoutColumnSplitting, "-");
+
+    // Construct the classes attribute for the current row
+    $layoutRowClass =
+      "class=\"" .
+      $layoutColumnSplitting .
+      " " .
+      $layoutRow->rowClasses() .
+      " " .
+      $rowVerticalPaddingClass .
+      "\"";
+
+    // Construct the style attribute for the current row
+    $layoutRowStyle = $layoutRow->rowBackgroundColor()
+      ? "style=\"background-color: " .
+        $layoutRow->rowBackgroundColor()->toColor("rgb") .
+        ";\""
+      : "";
+
+    // Add the row data to the array
+    $layoutRowsData[] = [
+      "layoutRowId" => $layoutRowId,
+      "layoutRowClass" => $layoutRowClass,
+      "layoutRowStyle" => $layoutRowStyle,
+      "layout" => $layoutRow,
+    ];
+  }
+
+  /**
+   * ---------------------------------------------------------------------------
+   * Return the variables to the snippet
+   * ---------------------------------------------------------------------------
+   */
+  return [
+    "layoutRowsData" => $layoutRowsData,
+  ];
+};
--- site/snippets/page-builder-default.php
+++ site/snippets/page-builder-default.php
@@ -9,61 +9,38 @@
  * Plugin details: https://github.com/lukaskleinschmidt/kirby-snippet-controller
  * 
  * Receives variables from snippet controller:
- * - ???
+ * - $layoutRowsData
  * =============================================================================
  */
 ?>
-      <?php foreach ($page->mainContent()->toLayouts() as $layout): ?>
-        <!-- Row -->
-        <?php switch ($layout->rowVerticalPadding()) {
-          case "small":
-            $rowVerticalPaddingOutput = "py-3";
-            break;
-          case "medium":
-            $rowVerticalPaddingOutput = "py-6";
-            break;
-          case "large":
-            $rowVerticalPaddingOutput = "py-12";
-            break;
-          default:
-            $rowVerticalPaddingOutput = "py-0";
-        } ?>
-        <section
-          <?= $layout->rowId()->isNotEmpty()
-            ? " id=\"" . $layout->rowId() . "\""
-            : "" ?>
-          <?= $layout->rowClasses()->isNotEmpty()
-            ? " class=\"" . $layout->rowClasses() . "\""
-            : "" ?>
-          class="<?= $rowVerticalPaddingOutput ?> <?= $layout->rowBackgroundColor() ?>"
-        >
-          <!--Inner row container -->
-          <div class="<?= $layout->rowWidth() ?> grid grid-cols-12 gap-6">
-          <?php foreach ($layout->columns() as $column): ?>
-            <!-- Column -->
-            <?php switch ($column->width()) {
-              case "1/2":
-                $columnClassOutput = "col-span-full md:col-span-6";
-                break;
-              case "1/3":
-                $columnClassOutput =
-                  "col-span-full md:col-span-6 lg:col-span-4";
-                break;
-              case "2/3":
-                $columnClassOutput =
-                  "col-span-full md:col-span-6 lg:col-span-8";
-                break;
-              case "1/4":
-                $columnClassOutput =
-                  "col-span-full md:col-span-6 lg:col-span-3";
-                break;
-              default:
-                $columnClassOutput = "col-span-full";
-            } ?>
-            <div class="<?= $columnClassOutput ?> bg-neutral-500">
-              <?= $column->blocks() ?>
-            </div>
-          <?php endforeach; ?>
-          </div>
-        </section>
+<?php foreach ($layoutRowsData as $layoutRow): ?>
+  <section <?= $layoutRow["layoutRowId"] ?> <?= $layoutRow[
+   "layoutRowClass"
+ ] ?> <?= $layoutRow["layoutRowStyle"] ?>>
+    <!--Inner row container -->
+    <div class="<?= $layoutRow[
+      "layout"
+    ]->rowWidth() ?> grid grid-cols-12 gap-6 md:[&_>_.column-width-1\/3:nth-child(3)]:col-start-4 lg:[&_>_.column-width-1\/3:nth-child(3)]:col-start-auto [.column-splitting-1\/4-1\/2-1\/4_>_&_>_.column-width-1\/4.empty-column]:hidden lg:[.column-splitting-1\/4-1\/2-1\/4_>_&_>_.column-width-1\/4.empty-column]:block md:[.column-splitting-1\/4-1\/2-1\/4_>_&_>_.column-width-1\/4]:col-start-4 lg:[.column-splitting-1\/4-1\/2-1\/4_>_&_>_.column-width-1\/4]:col-start-auto md:[:is(.column-splitting-1\/4-1\/2-1\/4,_.column-splitting-1\/4-1\/4-1\/2,_.column-splitting-1\/2-1\/4-1\/4)_>_&_>_.column-width-1\/2]:col-span-full lg:[:is(.column-splitting-1\/4-1\/2-1\/4,_.column-splitting-1\/4-1\/4-1\/2,_.column-splitting-1\/2-1\/4-1\/4)_>_&_>_.column-width-1\/2]:col-span-6">
+      <?php $columnWidthClasses = [
+        "1/2" => "column-width-1/2 col-span-full md:col-span-6",
+        "1/3" => "column-width-1/3 col-span-full md:col-span-6 lg:col-span-4",
+        "2/3" => "column-width-2/3 col-span-full md:col-span-6 lg:col-span-8",
+        "1/4" => "column-width-1/4 col-span-full md:col-span-6 lg:col-span-3",
+      ]; ?>
+      <?php foreach ($layoutRow["layout"]->columns() as $layoutColumn): ?>
+        <!-- Column -->
+        <?php
+        $columnClassOutput = $layoutColumn->blocks()->isEmpty()
+          ? "empty-column "
+          : "";
+        $columnClassOutput .=
+          $columnWidthClasses[$layoutColumn->width()] ??
+          "column-width-" . $layoutColumn->width() . " col-span-full";
+        ?>
+        <div class="<?= $columnClassOutput ?>">
+          <?= $layoutColumn->blocks() ?>
+        </div>
       <?php endforeach; ?>
+    </div>
+  </section>
+<?php endforeach; ?>
