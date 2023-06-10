08e6c8ade70db9a5b81f0d9b18d0905f1bb0f971
Michael van Laar <michael@van-laar.de>
2023-06-10 17:53:02 +0200

    🎨 style(footer.php): change margin size of footer menu items The margin size of the footer menu items has been changed from `me-6` to `me-medium` to improve the visual appearance of the footer.

--- site/snippets/base/footer.php
+++ site/snippets/base/footer.php
@@ -15,14 +15,14 @@
     <!-- Row -->
     <footer
       id="page-footer"
-      class="w-full bg-neutral-600 py-6 dark:bg-neutral-900 print:hidden"
+      class="w-full bg-neutral-600 py-medium dark:bg-neutral-900 print:hidden"
     >
       <!-- Inner row container -->
       <div class="row-container-default flex flex-col items-center justify-between text-white md:flex-row md:items-stretch">
         <nav class="mb-3 flex items-center md:mb-0">
           <ul class="flex">
             <?php foreach ($footerMenuItems as $menuItem): ?>
-              <li class="me-6">
+              <li class="me-medium">
                 <a
                   href="<?= $menuItem["url"] ?>"
                   target="<?= $menuItem["target"] ?>"
