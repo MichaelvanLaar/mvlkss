1d054c8bba9a08100658a3af9c6be2362776e01d
Michael van Laar <michael@van-laar.de>
2023-06-11 02:56:43 +0200

    🆕 feat(highlightjscss.js): add import statement for highlightjs.css to make it available for webpack This commit adds an import statement for the highlightjs.css file to the highlightjscss.js file. This is necessary to make the CSS file available for webpack.

---
+++ src/js/highlightjscss.js
@@ -0,0 +1,9 @@
+/**
+ * =============================================================================
+ * This file is required to make the highlightjs.css file available for webpack.
+ *
+ * DON’T REMOVE THIS FILE!
+ * =============================================================================
+ */
+
+import "../css/highlightjs.css";
