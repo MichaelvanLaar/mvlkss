67bf97b101b1a0ed8628f5efe5f7a3152894369c
Michael van Laar <michael@van-laar.de>
2023-06-20 01:55:46 +0200

    🔨 refactor(grid): replace hardcoded CSS classes with constants and variables The hardcoded CSS classes for padding, gap, and column width have been replaced with constants and variables to improve maintainability and readability of the code. The constants are defined in the `grid.php` file and the variables are set in the `foreach` loop that iterates over the grid layout rows and columns. The background color field has been extended to use a site-color-select field, which provides a selection of pre-defined brand colors to choose from. The background color is now set using CSS variables, which allows for more flexibility and easier customization.

--- site/snippets/blocks/grid.php
+++ site/snippets/blocks/grid.php
@@ -11,56 +11,13 @@
  * -----------------------------------------------------------------------------
  */
 
-// Define class mapping as constants to avoid reinitialization
-
-// Set padding values for the “small”, “medium” and “large” options of the
+// Set column width classes for the “1/1”, “1/2” and “1/3” options of the
 // respective layout settings field using Tailwind CSS classes
-define("GRID_ROW_PADDING_TOP_CLASSES", [
-  "none" => "pt-0",
-  "small" => "pt-small",
-  "medium" => "pt-medium",
-  "large" => "pt-large",
-  "xlarge" => "pt-xlarge",
-]);
-define("GRID_ROW_PADDING_BOTTOM_CLASSES", [
-  "none" => "pb-0",
-  "small" => "pb-small",
-  "medium" => "pb-medium",
-  "large" => "pb-large",
-  "xlarge" => "pb-xlarge",
-]);
-define("GRID_ROW_PADDING_START_CLASSES", [
-  "none" => "ps-0",
-  "small" => "ps-small",
-  "medium" => "ps-medium",
-  "large" => "ps-large",
-  "xlarge" => "ps-xlarge",
-]);
-define("GRID_ROW_PADDING_END_CLASSES", [
-  "none" => "pe-0",
-  "small" => "pe-small",
-  "medium" => "pe-medium",
-  "large" => "pe-large",
-  "xlarge" => "pe-xlarge",
-]);
-
-// Set vertical gap values for the “small”, “medium” and “large” options of the
-// respective layout settings field using Tailwind CSS classes
-define("GRID_ROW_GAP_CLASSES", [
-  "none" => "gap-0",
-  "small" => "gap-small",
-  "medium" => "gap-medium",
-  "large" => "gap-large",
-  "xlarge" => "gap-xlarge",
-]);
-
-// Set column width classes for the “1/1”, “1/2” and “1/3” of the respective
-// layout settings field using Tailwind CSS classes
-define("GRID_COLUMN_WIDTH_CLASSES", [
+$gridColumnWidthClasses = [
   "1/1" => "col-span-full",
   "1/2" => "col-span-3",
   "1/3" => "col-span-2",
-]);
+];
 
 /**
  * -----------------------------------------------------------------------------
@@ -72,53 +29,110 @@ define("GRID_COLUMN_WIDTH_CLASSES", [
 foreach ($block->grid()->toLayouts() as $gridLayoutRow): ?>
 
   <?php
-  // Fetch and prepare class strings for the row
-  $gridRowClasses = $gridLayoutRow->gridRowClasses()->isNotEmpty()
-    ? $gridLayoutRow->gridRowClasses()
-    : "";
-  $gridRowGap =
-    GRID_ROW_GAP_CLASSES[(string) $gridLayoutRow->gridRowGap()] ?? "gap-0";
-  $gridRowPaddingTop =
-    GRID_ROW_PADDING_TOP_CLASSES[
-      (string) $gridLayoutRow->gridRowPaddingTop()
-    ] ?? "pt-0";
-  $gridRowPaddingBottom =
-    GRID_ROW_PADDING_BOTTOM_CLASSES[
-      (string) $gridLayoutRow->gridRowPaddingBottom()
-    ] ?? "pb-0";
-  $gridRowPaddingStart =
-    GRID_ROW_PADDING_START_CLASSES[
-      (string) $gridLayoutRow->gridRowPaddingStart()
-    ] ?? "ps-0";
-  $gridRowPaddingEnd =
-    GRID_ROW_PADDING_END_CLASSES[
-      (string) $gridLayoutRow->gridRowPaddingEnd()
-    ] ?? "pe-0";
-
-  // Prepare other row attributes
+  // Construct the ID attribute for the current grid row
   $gridRowIdAttribute = $gridLayoutRow->gridRowId()->isNotEmpty()
     ? " id=\"{$gridLayoutRow->gridRowId()}\""
     : "";
-  $gridRowStyleAttribute = $gridLayoutRow->gridRowBackgroundColor()
-    ? "style=\"background-color: {$gridLayoutRow->gridRowBackgroundColor()->toColor(
-      "rgb"
-    )};\""
+
+  // Set the gap related CSS class for the current grid row
+  $gridRowGapClass =
+    GAP_CLASSES[(string) $gridLayoutRow->gridRowGap()] ?? "gap-0";
+
+  // Set the top padding related CSS class for the current grid row
+  $gridRowPaddingTopClass =
+    PADDING_TOP_CLASSES[(string) $gridLayoutRow->gridRowPaddingTop()] ?? "pt-0";
+
+  // Set the bottom padding related CSS class for the current grid row
+  $gridRowPaddingBottomClass =
+    PADDING_BOTTOM_CLASSES[(string) $gridLayoutRow->gridRowPaddingBottom()] ??
+    "pb-0";
+
+  // Set the start padding related CSS class for the current grid row
+  $gridRowPaddingStartClass =
+    PADDING_START_CLASSES[(string) $gridLayoutRow->gridRowPaddingStart()] ??
+    "ps-0";
+
+  // Set the end padding related CSS class for the current grid row
+  $gridRowPaddingEndClass =
+    PADDING_END_CLASSES[(string) $gridLayoutRow->gridRowPaddingEnd()] ?? "pe-0";
+
+  // Set the background color related CSS class for the current grid row
+  $gridRowBackgroundColorClasses = $gridLayoutRow
+    ->gridRowBackgroundColor()
+    ->isNotEmpty()
+    ? "bg-[var(--grid-row-background-color-light-mode)] dark:bg-[var(--grid-row-background-color-dark-mode)]"
     : "";
+
+  // Construct the classes attribute for the current grid row
+  $gridRowClasses = [
+    $gridLayoutRow->gridRowClasses(),
+    "grid",
+    "grid-cols-6",
+    $gridRowGapClass,
+    $gridRowPaddingTopClass,
+    $gridRowPaddingBottomClass,
+    $gridRowPaddingStartClass,
+    $gridRowPaddingEndClass,
+    $gridRowBackgroundColorClasses,
+  ];
+  $gridRowClassAttribute = "class=\"" . implode(" ", $gridRowClasses) . "\"";
+
+  // Construct the style attribute for the current grid row
+  if ($gridLayoutRow->gridRowBackgroundColor()->isNotEmpty()) {
+    $gridRowStyleAttribute =
+      "style=\"--grid-row-background-color-light-mode: " .
+      SITE_COLORS[$gridLayoutRow->gridRowBackgroundColor()->value()]["lightMode"] .
+      "; --grid-row-background-color-dark-mode: " .
+      SITE_COLORS[$gridLayoutRow->gridRowBackgroundColor()->value()]["darkMode"] .
+      ";\"";
+  } else {
+    $gridRowStyleAttribute = "";
+  }
   ?>
 
   <!-- Grid Row -->
   <div
-    class="<?= "$gridRowClasses grid grid-cols-6 $gridRowGap $gridRowPaddingTop $gridRowPaddingBottom $gridRowPaddingStart $gridRowPaddingEnd" ?>"
     <?= $gridRowIdAttribute ?>
+    <?= $gridRowClassAttribute ?>
     <?= $gridRowStyleAttribute ?>
   >
     <?php foreach ($gridLayoutRow->columns() as $gridLayoutColumn): ?>
       <!-- Grid Column -->
-      <div class="<?= GRID_COLUMN_WIDTH_CLASSES[$gridLayoutColumn->width()] ??
-        "col-span-full" ?> prose prose-neutral max-w-none dark:prose-invert">
+      <?php
+      $gridColumnClassOutput = "";
+      $gridColumnClassOutput .=
+        $gridColumnWidthClasses[$gridLayoutColumn->width()] ?? "col-span-full";
+      if ($gridLayoutRow->gridRowBackgroundColor()->isNotEmpty()) {
+        $gridRowBackgroundColorValue = $gridLayoutRow
+          ->gridRowBackgroundColor()
+          ->value();
+        $gridContrastColorForLightMode =
+          SITE_COLORS[$gridRowBackgroundColorValue]["contrastForLightMode"];
+        $gridContrastColorForDarkMode =
+          SITE_COLORS[$gridRowBackgroundColorValue]["contrastForDarkMode"];
+        switch ($gridContrastColorForLightMode) {
+          case "#000000":
+            $gridColumnClassOutput .= " prose-black";
+            break;
+          case "#ffffff":
+            $gridColumnClassOutput .= " prose-white";
+            break;
+        }
+        switch ($gridContrastColorForDarkMode) {
+          case "#000000":
+            $gridColumnClassOutput .= " dark:prose-black";
+            break;
+          case "#ffffff":
+            $gridColumnClassOutput .= " dark:prose-white";
+            break;
+        }
+      } else {
+        $gridColumnClassOutput .= " prose-neutral dark:prose-invert";
+      }
+      ?>
+      <div class="<?= $gridColumnClassOutput ?> prose max-w-none">
         <?= $gridLayoutColumn->blocks() ?>
       </div>
     <?php endforeach; ?>
   </div>
-
 <?php endforeach; ?>
