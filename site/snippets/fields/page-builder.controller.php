be6f092af0bd66055cbb86ac403f7f93b1c0d8f7
Michael van Laar <michael@van-laar.de>
2023-07-14 01:21:36 +0200

    ✨ feat(page-builder): add background image options to page builder 🔨 refactor(page-builder.controller): optimize code for readability and performance
    The page builder now supports background images, with options for image size, position, and repeat. This provides more flexibility and customization options for the page layout. The code in the page builder controller has been refactored to improve readability and performance. This includes extracting repeated code into helper functions, optimizing the retrieval of site constants, and improving the construction of CSS classes and styles.

--- site/snippets/fields/page-builder.controller.php
+++ site/snippets/fields/page-builder.controller.php
@@ -14,36 +14,38 @@
 return function ($page) {
   $layoutRows = $page->pageBuilder()->toLayouts();
   $layoutRowsData = [];
+  $siteConstants = option("site-constants");
+  $spacingUtilityClasses = $siteConstants["spacing-utility-classes"];
 
   foreach ($layoutRows as $layoutRow) {
     // Construct the ID attribute for the current row
-    $layoutRowIdAttribute = $layoutRow->rowId()->isNotEmpty()
-      ? sprintf("id=\"%s\"", $layoutRow->rowId())
+    $rowId = $layoutRow->rowId();
+    $layoutRowIdAttribute = $rowId->isNotEmpty()
+      ? sprintf("id=\"%s\"", $rowId)
       : "";
 
-    // Set the top padding related CSS class for the current row
+    // Set the top and bottom padding related CSS class for the current row
+    $rowPaddingTopValue = (string) $layoutRow->rowPaddingTop();
+    $rowPaddingBottomValue = (string) $layoutRow->rowPaddingBottom();
     $rowPaddingTopClass =
-      option("site-constants")["spacing-utility-classes"]["padding-top"][
-        (string) $layoutRow->rowPaddingTop()
-      ] ?? "pt-0";
-
-    // Set the bottom padding related CSS class for the current row
+      $spacingUtilityClasses["padding-top"][$rowPaddingTopValue] ?? "pt-0";
     $rowPaddingBottomClass =
-      option("site-constants")["spacing-utility-classes"]["padding-bottom"][
-        (string) $layoutRow->rowPaddingBottom()
-      ] ?? "pb-0";
+      $spacingUtilityClasses["padding-bottom"][$rowPaddingBottomValue] ??
+      "pb-0";
 
     // Set the column splitting related CSS class for the current row
-    $layoutColumnSplitting = "column-splitting-";
-    foreach ($layoutRow->columns() as $layoutColumn) {
-      $layoutColumnSplitting .= $layoutColumn->width() . "-";
-    }
-    $layoutColumnSplitting = rtrim($layoutColumnSplitting, "-");
+    $layoutColumnSplitting = buildColumnSplitting($layoutRow);
 
     // Set the background color related CSS class for the current row
-    $rowBackgroundColorClasses = $layoutRow->rowBackgroundColor()->isNotEmpty()
+    $rowBackgroundColor = $layoutRow->rowBackgroundColor();
+    $rowBackgroundColorExists = $rowBackgroundColor->isNotEmpty();
+    $rowBackgroundColorValue = $rowBackgroundColor->value();
+    $rowBackgroundColorClasses = $rowBackgroundColorExists
       ? "bg-[var(--row-background-color-light-mode)] dark:bg-[var(--row-background-color-dark-mode)]"
       : "";
+
+    // Set the background image related CSS classes for the current row
+    $rowBackgroundImageClasses = buildBackgroundImageClasses($layoutRow);
 
     // Construct the classes attribute for the current row
     $layoutRowClasses = [
@@ -52,24 +54,20 @@ return function ($page) {
       $rowPaddingTopClass,
       $rowPaddingBottomClass,
       $rowBackgroundColorClasses,
+      $rowBackgroundImageClasses,
     ];
     $layoutRowClassAttribute = sprintf(
       "class=\"%s\"",
-      implode(" ", $layoutRowClasses),
+      implode(" ", $layoutRowClasses)
     );
 
     // Construct the style attribute for the current row
-    $layoutRowStyleAttribute = $layoutRow->rowBackgroundColor()->isNotEmpty()
-      ? sprintf(
-        "style=\"--row-background-color-light-mode: %s; --row-background-color-dark-mode: %s;\"",
-        option("site-constants")["site-colors"][
-          $layoutRow->rowBackgroundColor()->value()
-        ]["lightMode"],
-        option("site-constants")["site-colors"][
-          $layoutRow->rowBackgroundColor()->value()
-        ]["darkMode"],
-      )
-      : "";
+    $layoutRowStyleAttribute = buildStyleAttribute(
+      $layoutRow,
+      $rowBackgroundColorValue,
+      $siteConstants,
+      $rowBackgroundColorExists
+    );
 
     // Add the row data to the array
     $layoutRowsData[] = [
@@ -82,7 +80,138 @@ return function ($page) {
   }
 
   // Return the variables to the snippet
-  return [
-    "layoutRowsData" => $layoutRowsData,
-  ];
+  return ["layoutRowsData" => $layoutRowsData];
 };
+
+/**
+ * -----------------------------------------------------------------------------
+ * Helper functions
+ * -----------------------------------------------------------------------------
+ */
+
+function buildColumnSplitting($layoutRow) {
+  $layoutColumnSplitting = "column-splitting-";
+  foreach ($layoutRow->columns() as $layoutColumn) {
+    $layoutColumnSplitting .= $layoutColumn->width() . "-";
+  }
+  return rtrim($layoutColumnSplitting, "-");
+}
+
+function buildBackgroundImageClasses($layoutRow) {
+  $rowBackgroundImageClasses = "";
+
+  // Early return the empty string if the row has no background image
+  if ($layoutRow->rowBackgroundImage()->isEmpty()) {
+    return $rowBackgroundImageClasses;
+  }
+
+  // Set the background image size related CSS class
+  $rowBackgroundImageSize = $layoutRow->rowBackgroundImageSize();
+  $rowBackgroundImageClasses .=
+    $rowBackgroundImageSize == "cover" ? "bg-cover" : "bg-auto";
+
+  // Set the background image position related CSS class
+  $backgroundImagePositionMap = [
+    "top-left" => " bg-left-top",
+    "top-center" => " bg-top",
+    "top-right" => " bg-right-top",
+    "middle-left" => " bg-left",
+    "middle-center" => " bg-center",
+    "middle-right" => " bg-right",
+    "bottom-left" => " bg-left-bottom",
+    "bottom-center" => " bg-bottom",
+    "bottom-right" => " bg-right-bottom",
+  ];
+  $rowBackgroundImageClasses .=
+    $backgroundImagePositionMap[
+      (string) $layoutRow->rowBackgroundImagePosition()
+    ] ?? "";
+
+  // Set the background image repeat related CSS class
+  $backgroundImageRepeatMap = [
+    "no-repeat" => " bg-no-repeat",
+    "repeat" => " bg-repeat",
+    "repeat-x" => " bg-repeat-x",
+    "repeat-y" => " bg-repeat-y",
+  ];
+  $rowBackgroundImageClasses .=
+    $backgroundImageRepeatMap[
+      (string) $layoutRow->rowBackgroundImageRepeat()
+    ] ?? "";
+
+  // Return the background image related CSS classes
+  return $rowBackgroundImageClasses;
+}
+
+function buildStyleAttribute(
+  $layoutRow,
+  $rowBackgroundColorValue,
+  $siteConstants,
+  $rowBackgroundColorExists
+) {
+  // Open the style attribute
+  $layoutRowStyleAttribute = "style=\"";
+
+  // Set the background color related CSS custom properties
+  if ($rowBackgroundColorExists) {
+    $layoutRowStyleAttribute .= sprintf(
+      "--row-background-color-light-mode: %s; --row-background-color-dark-mode: %s;",
+      $siteConstants["site-colors"][$rowBackgroundColorValue]["lightMode"],
+      $siteConstants["site-colors"][$rowBackgroundColorValue]["darkMode"]
+    );
+  }
+
+  // Close and early return the style attribute if the row has no background
+  // image
+  if ($layoutRow->rowBackgroundImage()->isEmpty()) {
+    return $layoutRowStyleAttribute . "\"";
+  }
+
+  $rowBackgroundImageFile = $layoutRow->rowBackgroundImage()->toFile();
+  $rowBackgroundImageFileExtension = $rowBackgroundImageFile->extension();
+
+  // Add the background image related CSS rules to the style attribute
+  $layoutRowStyleAttribute .= buildBackgroundImageUrl(
+    $rowBackgroundImageFile,
+    $rowBackgroundImageFileExtension
+  );
+
+  // Close and return the style attribute
+  return $layoutRowStyleAttribute . "\"";
+}
+
+function buildBackgroundImageUrl(
+  $rowBackgroundImageFile,
+  $rowBackgroundImageFileExtension
+) {
+  // Set the background image fallback for older browsers
+  $rowBackgroundImageStyle = sprintf(
+    " background-image: url('%s');",
+    $rowBackgroundImageFile->url()
+  );
+
+  // Add modern file formats (AVIF and/or WebP) of the background image for
+  // modern browsers, in case the background image is a JPG or PNG file
+  switch ($rowBackgroundImageFileExtension) {
+    case "png":
+      $rowBackgroundImageStyle .= sprintf(
+        " background-image: url('%s');",
+        $rowBackgroundImageFile->thumb("png->webp")->url()
+      );
+      break;
+    case "jpg":
+    case "jpeg":
+      $rowBackgroundImageStyle .= sprintf(
+        " background-image: url('%s');",
+        $rowBackgroundImageFile->thumb("jpg->webp")->url()
+      );
+      $rowBackgroundImageStyle .= sprintf(
+        " background-image: url('%s');",
+        $rowBackgroundImageFile->thumb("jpg->avif")->url()
+      );
+      break;
+  }
+
+  // Return the background image related CSS rules
+  return $rowBackgroundImageStyle;
+}
