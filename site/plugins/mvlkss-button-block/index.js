1e395361c49e996c991ad70bf309003f878c963e
Michael van Laar <michael@van-laar.de>
2023-09-05 23:03:48 +0200

    🎨 style(mvlkss-button-block): add filled button style and change alignment class name 🔨 refactor(mvlkss-button-block): add button style class to button element in index.js The CSS changes introduce a new filled button style, which provides a solid background color for better visibility and contrast. The alignment class name has been changed from 'right' to 'end' to better reflect the directionality of the content. In the JavaScript file, a new class has been added to the button element to support different button styles, enhancing the flexibility of the button design.

--- site/plugins/mvlkss-button-block/index.js
+++ site/plugins/mvlkss-button-block/index.js
@@ -8,7 +8,7 @@ panel.plugin("mvlkss/button-block", {
   blocks: {
     mvlkssbutton: `
       <div :class="\`button-align-\${content.align}\`">
-        <button type="button" @click="open" :class="\`button-width-\${content.width}\`">
+        <button type="button" @click="open" :class="[\`button-width-\${content.width}\`, \`button-style-\${content.style}\`]">
           {{ content.text }}
         </button>
       </div>
