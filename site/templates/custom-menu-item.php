39e99421dac18c42aed76d4d8cd2dd082d362caa
Michael van Laar <michael@van-laar.de>
2023-12-17 12:18:02 +0100

    ✨ feat(custom-menu-item.php): add new template for custom menu item to support redirection to URL set in menuItemLink field

---
+++ site/templates/custom-menu-item.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * =============================================================================
+ * Not Used Default Template for Custom Menu Item
+ * =============================================================================
+ */
+
+// Redirect to the URL set in the menuItemLink field
+go($page->menuItemLink()->toUrl(), 301);
